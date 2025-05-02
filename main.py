44ba0c808e756f5fdf761970af932d5b9631ddb3
Plazmer <praw2003@gmail.com>
2025-05-03 00:54:01 +0500

    304

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from bottle import route, run
 
+
+@route('/health')
+def index():
+    return 'HEALTHY'
+
+
+@route('/ready')
+def index():
+    return 'READY'
+
+
 @route('/')
 def index():
-    return '<b>Hello World! at 10:21:27</b>!'
+    return '<b>Hello World! at 10:49:27</b>!'
 
 run(host='0.0.0.0', port=8080)
